642322995399bbd6a46428543bab2f7ad0a9012e
VaNek2001 <ya.kurochkin2001@yandex.ru>
2022-12-06 20:27:24 +0500

    v. 0.3

--- ToDoList/MainViewModel.cs
+++ ToDoList/MainViewModel.cs
@@ -5,36 +5,85 @@ using System;
 
 namespace ToDoList;
 
-public class Item
-{
-    public string Title;
-    public string Color;
-}
-
 public class MainViewModel : BindableObject
 {
+    private int _index;
     private string _color;
-    public string color { get => _color;
-        set {
+    public string color
+    {
+        get => _color;
+        set
+        {
             _color = value;
             OnPropertyChanged(nameof(color));
         }
     }
 
+    private bool _changeMode = false;
+    public bool ChangeMode
+    {
+        get => _changeMode;
+        set
+        {
+            _changeMode = value;
+            OnPropertyChanged(nameof(ChangeMode));
+        }
+    }
+    private bool _changeModeInverse = true;
+    public bool ChangeModeInverse
+    {
+        get => _changeModeInverse;
+        set
+        {
+            _changeModeInverse = value;
+            OnPropertyChanged(nameof(ChangeModeInverse));
+        }
+    }
+    private string _activeText = "";
+    public string ActiveText
+    {
+        get => _activeText;
+        set
+        {
+            _activeText = value;
+            OnPropertyChanged(nameof(ActiveText));
+        }
+    }
+
     public MainViewModel()
     {
+        _color = "red";
         AddItemCommand = new Command<string>(x =>
         {
             var item = new ToDoItem(x, color);
             ToDoItems.Add(item);
+            ActiveText = "";
         }
-        ,x => string.IsNullOrWhiteSpace(x) == false);
+        , x => string.IsNullOrWhiteSpace(x) == false);
 
         DeleteItemCommand = new Command<ToDoItem>(x => ToDoItems.Remove(x));
-        
+        ChangeCommand = new Command<ToDoItem>(x =>
+        {
+            ChangeMode = !ChangeMode;
+            ChangeModeInverse = !ChangeModeInverse;
+            _index = ToDoItems.IndexOf(x);
+            ActiveText = x.Title;
+        });
+        SaveCommand = new Command<string>(title =>
+        {
+            ToDoItems[_index].Title = title;
+            ToDoItems[_index].Color = Color.Parse(color);
+            ChangeMode = !ChangeMode;
+            ChangeModeInverse = !ChangeModeInverse;
+            ActiveText = "";
+        }
+        , title => string.IsNullOrWhiteSpace(title) == false);
+
     }
     public ICommand AddItemCommand { get; }
     public ICommand DeleteItemCommand { get; }
+    public ICommand ChangeCommand { get; }
+    public ICommand SaveCommand { get; }
 
     public ObservableCollection<ToDoItem> ToDoItems { get; } = new ObservableCollection<ToDoItem>();
 }
@@ -89,93 +138,3 @@ public class ToDoItem : BindableObject
         return Title;
     }
 }
-
-//private string _test;
-//private int _count;
-
-//public string Test { get { return _test; } set { if (_test == value) return; _test = value; OnPropertyChanged(nameof(Test)); } }
-//public int Count { get { return _count; } set { if (_count == value) return; _count = value; OnPropertyChanged(nameof(Count)); } }
-
-//public ICommand CountCommand { get; }
-//public ICommand ValueCommand { get; }
-//public ICommand DecountCommand { get; }
-
-//public MainViewModel()
-//{
-//    CountCommand = new Command(() => Count++);
-//    ValueCommand = new Command<string>(x => { Debug.WriteLine(x); Debug.WriteLine(x.GetType()); Count = Count + int.Parse(x); });
-//    DecountCommand = new Command(() => Count--, () => Count > 0);
-//}
-
-
-
-
-//    {
-//        AddItemCommand = new Command<Item>(x =>
-//        {
-//            Item item1 = x as Item;
-//            if(item1 != null)
-//            {
-//                Console.WriteLine(item1.Title + " " + item1.Color);
-//                var item = new ToDoItem(item1.Title, item1.Color);
-//ToDoItems.Add(item);
-//            }
-//        },x =>
-//          {
-//              Item item = x as Item;
-//              if (item != null)
-//                  return string.IsNullOrWhiteSpace(item.Title) == false;
-//              return false;
-//          });
-
-
-
-//public MainViewModel()
-//{
-//    AddItemCommand = new Command<string>(title =>
-//    {
-//        var item = new ToDoItem(title);
-//        ToDoItems.Add(item);
-//    }, title => string.IsNullOrWhiteSpace(title) == false);
-//}
-
-//public ICommand AddItemCommand { get; }
-
-//public ObservableCollection<ToDoItem> ToDoItems { get; } = new ObservableCollection<ToDoItem>();
-
-//public class ToDoItem : BindableObject
-//{
-//    public ToDoItem(string title)
-//    {
-//        _title = title;
-//    }
-
-//    private bool _done;
-//    public bool Done
-//    {
-//        get => _done;
-//        set
-//        {
-//            if (_done == value) return;
-//            _done = value;
-//            OnPropertyChanged(nameof(Done));
-//        }
-//    }
-
-//    private string _title;
-//    public string Title
-//    {
-//        get => _title;
-//        set
-//        {
-//            if (_title == value) return;
-//            _title = value;
-//            OnPropertyChanged(nameof(Title));
-//        }
-//    }
-
-//    public override string ToString()
-//    {
-//        return Title;
-//    }
-//}
